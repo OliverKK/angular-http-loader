86213426a4e8642ee58a307dacb2cd63233456dc
OliverKK <oliver.kinderknecht@googlemail.com>
2014-09-17 17:51:39 +0200

    Added request type checker

--- app/src/js/httpMethodInterceptor.js
+++ app/src/js/httpMethodInterceptor.js
@@ -1,107 +1,139 @@
-/* global angular, _, console */
-
 /**
  * Http method interceptor. Broadcast events for show or hide the loader.
  */
-angular
-  .module('ng.httpLoader.httpMethodInterceptor', [])
+angular.module('ng.httpLoader.httpMethodInterceptor', [])
 
-  .provider('httpMethodInterceptor', function () {
-    var domains = [];
+.provider('httpMethodInterceptor', function () {
+  var domains = [];
+  var requestTypes = [];
+  var requestHeaderName = "";
 
-    /**
-     * Add domains to the white list
-     *
-     * @param {string} domain
-     * Added Domain to the white list domains collection
-     */
-    this.whitelistDomain = function (domain) {
-      domains.push(domain);
-    };
+  /**
+   * Add domains to the white list
+   *
+   * @param {string} domain
+   * Added Domain to the white list domains collection
+   */
+  this.whitelistDomain = function (domain) {
+    domains.push(domain);
+  };
 
-    this.$get = [
-      '$q',
-      '$rootScope',
-      function ($q, $rootScope) {
-        var numLoadings = 0;
+  /**
+   * Add request types to white list.
+   *
+   * @param {string} request type.
+   */
+  this.requestType = function (requestType) {
+    requestTypes.push(requestType);
+  };
 
-        /**
-         * Check if the url domain is on the whitelist
-         *
-         * @param {string} url
-         *
-         * @returns {boolean}
-         */
-        var isUrlOnWhitelist = function (url) {
-          for (var i = domains.length; i--;) {
-            if (url.indexOf(domains[i]) !== -1) {
-              return true;
-            }
+  /**
+   * Add header name.
+   *
+   * @param {string} header name.
+   */
+  this.headerName = function (headerName) {
+    requestHeaderName = headerName;
+  };
+
+  this.$get = [
+    '$q',
+    '$rootScope',
+    function ($q, $rootScope) {
+      var numLoadings = 0;
+
+      /**
+       * Check if the url domain is on the whitelist
+       *
+       * @param {string} url
+       *
+       * @returns {boolean}
+       */
+      var isUrlOnWhitelist = function (url) {
+        for (var i = domains.length; i--;) {
+          if (url.indexOf(domains[i]) !== -1) {
+            return true;
           }
+        }
+        return false;
+      };
 
-          return false;
-        };
+      /**
+       * Check if the request type is a allowed request type.
+       *
+       * @param {object} header
+       *
+       * @returns {boolean}
+       */
+      var isAllowedRequestType = function (headers) {
+        for (var i = requestTypes.length; i--;) {
+          if (headers[requestHeaderName].indexOf(requestTypes[i]) !== -1) {
+            return true;
+          }
+        }
+        return false;
+      };
 
+      /**
+       * Emit hide loader logic
+       *
+       * @param {object} config
+       * The response configuration
+       */
+      var checkAndHide = function (config) {
+        if (isUrlOnWhitelist(config.url) && isAllowedRequestType(config.headers) && (--numLoadings) === 0) {
+          $rootScope.$emit('loaderHide', config.method);
+        }
+      };
+
+      return {
         /**
-         * Emit hide loader logic
+         * Broadcast the loader show event
          *
          * @param {object} config
-         * The response configuration
+         *
+         * @returns {object|Promise}
          */
-        var checkAndHide = function (config) {
-          if (isUrlOnWhitelist(config.url) &&
-            (--numLoadings) === 0) {
-            $rootScope.$emit('loaderHide', config.method);
+        request: function (config) {
+
+          if (isUrlOnWhitelist(config.url) && isAllowedRequestType(config.headers)) {
+            numLoadings++;
+            $rootScope.$emit('loaderShow', config.method);
           }
-        };
 
-        return {
-          /**
-           * Broadcast the loader show event
-           *
-           * @param {object} config
-           *
-           * @returns {object|Promise}
-           */
-          request: function (config) {
-            if (isUrlOnWhitelist(config.url)) {
-              numLoadings++;
-              $rootScope.$emit('loaderShow', config.method);
-            }
+          return config || $q.when(config);
+        },
 
-            return config || $q.when(config);
-          },
+        /**
+         * Broadcast the loader hide event
+         *
+         * @param {object} response
+         *
+         * @returns {object|Promise}
+         */
+        response: function (response) {
+          checkAndHide(response.config);
 
-          /**
-           * Broadcast the loader hide event
-           *
-           * @param {object} response
-           *
-           * @returns {object|Promise}
-           */
-          response: function (response) {
-            checkAndHide(response.config);
+          return response || $q.when(response);
+        },
 
-            return response || $q.when(response);
-          },
+        /**
+         * Handle errors
+         *
+         * @param {object} response
+         *
+         * @returns {Promise}
+         */
+        responseError: function (response) {
+          checkAndHide(response.config);
 
-          /**
-           * Handle errors
-           *
-           * @param {object} response
-           *
-           * @returns {Promise}
-           */
-          responseError: function (response) {
-            checkAndHide(response.config);
+          return $q.reject(response);
+        }
+      };
+    }
+  ];
+})
 
-            return $q.reject(response);
-          }
-        };
-      }
-    ];
-  })
-
-  .config(['$httpProvider', function ($httpProvider) {
-    $httpProvider.interceptors.push('httpMethodInterceptor');
-  }]);
+.config(['$httpProvider', function ($httpProvider) {
+  $httpProvider.interceptors.push('httpMethodInterceptor');
+}]);
