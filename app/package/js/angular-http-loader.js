18a3a80ea3b2af91b9e0185e0ad24eb6c6fd20e1
OliverKK <oliver.kinderknecht@googlemail.com>
2014-09-19 13:58:19 +0200

    Usability improved

--- app/package/js/angular-http-loader.js
+++ app/package/js/angular-http-loader.js
@@ -1,9 +1,9 @@
 /* global angular, console */
+'use strict';
 
-angular
-  .module('ng.httpLoader', [
+angular.module('ng.httpLoader', [
     'ng.httpLoader.httpMethodInterceptor'
-  ])
+])
 
   .directive('ngHttpLoader', [
     '$rootScope',
@@ -32,6 +32,17 @@ angular
        *      title="Foo"
        *      methods="'GET'"
        *      template="example/loader.tpl.html"></div>
+       *
+       * Adding event names to catch [optional]
+       *
+       * <div ng-http-loader
+       *      title="Foo"
+       *      methods="'GET'"
+       *      template="example/loader.tpl.html"
+       *      beforeloadershow="$$BeforeLoaderShow"
+       *      afterloadershow="$$AfterLoaderShow"
+       *      beforeloaderhide="$$beforeLoaderhide"
+       *      afterloaderhide="$$AfterLoaderHide"></div>
        */
       return {
         /**
@@ -40,19 +51,38 @@ angular
          * @param {array|string} methods
          * @param {string} template
          * @param {string} title
-         * @param {number} time to live in seconds
-         * @param {string} The request type.
+         * @param {number} ttl time to live in seconds
+         * @param {string} beforeloadershow The event name before the loader shows up.
+         * @param {string} afterloadershow The event name after the scope variable to show the loader was set to true.
+         * @param {string} beforeloaderhide The event name before the loader will be hidden.
+         * @param {string} afterloaderhide The event name after the scope variable to show the loader was set to false.
          */
         scope: {
           methods: '@',
           template: '@',
           title: '@',
-          ttl: '@'
+          ttl: '@',
+          beforeloadershow: '@',
+          afterloadershow: '@',
+          beforeloaderhide: '@',
+          afterloaderhide: '@'
         },
         template: '<div class="http-loader__wrapper" ' +
           'ng-include="template" ' +
           'ng-show="showLoader"></div>',
         link: function ($scope) {
+
+          /**
+           * The custom event names to register.
+           */
+          var beforeLoaderShow =  $parse($scope.beforeloadershow)() || $scope.beforeloadershow;
+          var afterLoaderShow = $parse($scope.afterloadershow)() || $scope.afterloadershow;
+          var beforeLoaderHide = $parse($scope.beforeloaderhide)() || $scope.beforeloaderhide;
+          var afterLoaderHide = $parse($scope.afterloaderhide)() || $scope.afterloaderhide;
+
+          /**
+           * The methods to listen to.
+           */
           var methods = $parse($scope.methods)() || $scope.methods;
           methods = angular.isUndefined(methods) ? [] : methods;
           methods = angular.isArray(methods) ? methods : [methods];
@@ -60,11 +90,13 @@ angular
             methods[index] = method.toUpperCase();
           });
 
+          /**
+           * The time to live.
+           */
           var ttl = $parse($scope.ttl)() || $scope.ttl;
           ttl = angular.isUndefined(ttl) ? 0 : ttl;
           ttl = Number(ttl) * 1000;
           ttl = angular.isNumber(ttl) ? ttl : 0;
-
 
           // add minimal indexOf polyfill
           if (!Array.prototype.indexOf) {
@@ -81,12 +113,14 @@ angular
 
           /**
            * Loader is hidden by default
+           *
            * @type {boolean}
            */
           $scope.showLoader = false;
-
-          var timeoutId,
-            showLoader = $scope.showLoader;
+          var showLoader = $scope.showLoader;
+          var timeoutId;
+          var beforeEvent = '';
+          var afterEvent = '';
 
           /**
            * Toggle the show loader.
@@ -97,6 +131,19 @@ angular
            * @param {string} method
            */
           var toggleShowLoader = function (event, method) {
+            switch(event.name) {
+              case 'loaderShow':
+                beforeEvent = beforeLoaderShow
+                afterEvent = afterLoaderShow
+                break;
+              case 'loaderHide':
+                beforeEvent = beforeLoaderHide
+                afterEvent = afterLoaderHide
+                break;
+            }
+
+            if (beforeEvent !== '') { $rootScope.$broadcast(beforeEvent) } // Broadcasts a event before show or hide takes place
+
             if (methods.indexOf(method.toUpperCase()) !== -1) {
               showLoader = (event.name === 'loaderShow');
             } else if (methods.length === 0) {
@@ -105,8 +152,10 @@ angular
 
             if (ttl <= 0 || (!timeoutId && !showLoader)) {
               $scope.showLoader = showLoader;
+              if (afterEvent !== '') { $rootScope.$broadcast(afterEvent) } // Broadcasts a event before show or hide takes place
               return;
             } else if (timeoutId) {
+              if (afterEvent !== '') { $rootScope.$broadcast(afterEvent) } // Broadcasts a event before show or hide takes place
               return;
             }
 
@@ -115,6 +164,8 @@ angular
               if (!showLoader) {
                 $scope.showLoader = showLoader;
               }
+
+              if (afterEvent !== '') { $rootScope.$broadcast(afterEvent) } // Broadcasts a event before show or hide takes place
               timeoutId = undefined;
             }, ttl);
           };
@@ -133,139 +184,114 @@ angular
  */
 angular.module('ng.httpLoader.httpMethodInterceptor', [])
 
-.provider('httpMethodInterceptor', function () {
-  var domains = [];
-  var requestTypes = [];
-  var requestHeaderName = "";
+  .provider('httpMethodInterceptor', function () {
+    var configurations = []
+    var domains = [];
 
-  /**
-   * Add domains to the white list
-   *
-   * @param {string} domain
-   * Added Domain to the white list domains collection
-   */
-  this.whitelistDomain = function (domain) {
-    domains.push(domain);
-  };
+    /**
+     * Add route configuration.
+     * @param configObject The configuration object.
+     */
+    this.requestConfig = function(config) {
+      configurations.push(config);
+    }
 
-  /**
-   * Add request types to white list.
-   *
-   * @param {string} request type.
-   */
-  this.requestType = function (requestType) {
-    requestTypes.push(requestType);
-  };
+    this.$get = [
+      '$q',
+      '$rootScope',
+      function ($q, $rootScope) {
+        var numLoadings = 0;
 
-  /**
-   * Add header name.
-   *
-   * @param {string} header name.
-   */
-  this.headerName = function (headerName) {
-    requestHeaderName = headerName;
-  };
-
-  this.$get = [
-    '$q',
-    '$rootScope',
-    function ($q, $rootScope) {
-      var numLoadings = 0;
-
-      /**
-       * Check if the url domain is on the whitelist
-       *
-       * @param {string} url
-       *
-       * @returns {boolean}
-       */
-      var isUrlOnWhitelist = function (url) {
-        for (var i = domains.length; i--;) {
-          if (url.indexOf(domains[i]) !== -1) {
-            return true;
-          }
-        }
-        return false;
-      };
-
-      /**
-       * Check if the request type is a allowed request type.
-       *
-       * @param {object} header
-       *
-       * @returns {boolean}
-       */
-      var isAllowedRequestType = function (headers) {
-        for (var i = requestTypes.length; i--;) {
-          if(headers[requestHeaderName]) {
-            if (headers[requestHeaderName].indexOf(requestTypes[i]) !== -1) {
+        /**
+         * Checks if the url is allowed by the configuration.
+         *
+         * @param {string} url The url to check.
+         * @returns {boolean} True if the passed url is allowed, false otherwise.
+         */
+        var isUrlOnWhitelist = function (url) {
+          for (var i = configurations.length; i--;) {
+            if (url.indexOf(configurations[i]['url']) !== -1) {
               return true;
             }
           }
-        }
-        return false;
-      };
+          return false;
+        };
 
-      /**
-       * Emit hide loader logic
-       *
-       * @param {object} config
-       * The response configuration
-       */
-      var checkAndHide = function (config) {
-        if (isUrlOnWhitelist(config.url) && isAllowedRequestType(config.headers) && (--numLoadings) === 0) {
-          $rootScope.$emit('loaderHide', config.method);
-        }
-      };
-
-      return {
         /**
-         * Broadcast the loader show event
+         * Checks if the request is allowed by the configuration.
+         *
+         * @param {object} config The request configuration
+         * @returns {boolean} True if the request is allowed, false otherwise.
+         */
+        var isAllowedRequest = function (config) {
+          if (isUrlOnWhitelist(config.url)) {
+            for (var i = configurations.length; i--;) {
+              if (config.url.indexOf(configurations[i]['url']) !== -1 && (config.headers[configurations[i]['headerName']] === configurations[i]['requestType'])) {
+                return true;
+              }
+            }
+          }
+          return false;
+        };
+
+        /**
+         * Emit hide loader logic
          *
          * @param {object} config
-         *
-         * @returns {object|Promise}
+         * The response configuration
          */
-        request: function (config) {
-
-          if (isUrlOnWhitelist(config.url) && isAllowedRequestType(config.headers)) {
-            numLoadings++;
-            $rootScope.$emit('loaderShow', config.method);
+        var checkAndHide = function (config) {
+          if (isAllowedRequest(config) && (--numLoadings) === 0) {
+            $rootScope.$emit('loaderHide', config.method);
           }
+        };
 
-          return config || $q.when(config);
-        },
+        return {
+          /**
+           * Broadcast the loader show event
+           *
+           * @param {object} config
+           *
+           * @returns {object|Promise}
+           */
+          request: function (config) {
+            if (isAllowedRequest(config)) {
+              numLoadings++;
+              $rootScope.$emit('loaderShow', config.method);
+            }
+            return config || $q.when(config);
+          },
 
-        /**
-         * Broadcast the loader hide event
-         *
-         * @param {object} response
-         *
-         * @returns {object|Promise}
-         */
-        response: function (response) {
-          checkAndHide(response.config);
+          /**
+           * Broadcast the loader hide event
+           *
+           * @param {object} response
+           *
+           * @returns {object|Promise}
+           */
+          response: function (response) {
+            checkAndHide(response.config);
 
-          return response || $q.when(response);
-        },
+            return response || $q.when(response);
+          },
 
-        /**
-         * Handle errors
-         *
-         * @param {object} response
-         *
-         * @returns {Promise}
-         */
-        responseError: function (response) {
-          checkAndHide(response.config);
+          /**
+           * Handle errors
+           *
+           * @param {object} response
+           *
+           * @returns {Promise}
+           */
+          responseError: function (response) {
+            checkAndHide(response.config);
 
-          return $q.reject(response);
-        }
-      };
-    }
-  ];
-})
+            return $q.reject(response);
+          }
+        };
+      }
+    ];
+  })
 
-.config(['$httpProvider', function ($httpProvider) {
-  $httpProvider.interceptors.push('httpMethodInterceptor');
-}]);
+  .config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('httpMethodInterceptor');
+  }]);
