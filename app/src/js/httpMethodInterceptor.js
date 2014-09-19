687fc04d65f090d9ae90f62906ff0a12a51e3aed
OliverKK <oliver.kinderknecht@googlemail.com>
2014-09-19 14:01:35 +0200

    Update httpMethodInterceptor.js

--- app/src/js/httpMethodInterceptor.js
+++ app/src/js/httpMethodInterceptor.js
@@ -1,141 +1,121 @@
 /**
  * Http method interceptor. Broadcast events for show or hide the loader.
  */
+/* global angular, _, console */
+
+/**
+ * Http method interceptor. Broadcast events for show or hide the loader.
+ */
 angular.module('ng.httpLoader.httpMethodInterceptor', [])
 
-.provider('httpMethodInterceptor', function () {
-  var domains = [];
-  var requestTypes = [];
-  var requestHeaderName = "";
+  .provider('httpMethodInterceptor', function () {
+    var configurations = []
+    var domains = [];
 
-  /**
-   * Add domains to the white list
-   *
-   * @param {string} domain
-   * Added Domain to the white list domains collection
-   */
-  this.whitelistDomain = function (domain) {
-    domains.push(domain);
-  };
+    /**
+     * Add route configuration.
+     * @param configObject The configuration object.
+     */
+    this.requestConfig = function(config) {
+      configurations.push(config);
+    }
 
-  /**
-   * Add request types to white list.
-   *
-   * @param {string} request type.
-   */
-  this.requestType = function (requestType) {
-    requestTypes.push(requestType);
-  };
+    this.$get = [
+      '$q',
+      '$rootScope',
+      function ($q, $rootScope) {
+        var numLoadings = 0;
 
-  /**
-   * Add header name.
-   *
-   * @param {string} header name.
-   */
-  this.headerName = function (headerName) {
-    requestHeaderName = headerName;
-  };
-
-  this.$get = [
-    '$q',
-    '$rootScope',
-    function ($q, $rootScope) {
-      var numLoadings = 0;
-
-      /**
-       * Check if the url domain is on the whitelist
-       *
-       * @param {string} url
-       *
-       * @returns {boolean}
-       */
-      var isUrlOnWhitelist = function (url) {
-        for (var i = domains.length; i--;) {
-          if (url.indexOf(domains[i]) !== -1) {
-            return true;
-          }
-        }
-        return false;
-      };
-
-      /**
-       * Check if the request type is a allowed request type.
-       *
-       * @param {object} header
-       *
-       * @returns {boolean}
-       */
-      var isAllowedRequestType = function (headers) {
-        for (var i = requestTypes.length; i--;) {
-          if(headers[requestHeaderName]) {
-            if (headers[requestHeaderName].indexOf(requestTypes[i]) !== -1) {
+        /**
+         * Checks if the url is allowed by the configuration.
+         *
+         * @param {string} url The url to check.
+         * @returns {boolean} True if the passed url is allowed, false otherwise.
+         */
+        var isUrlOnWhitelist = function (url) {
+          for (var i = configurations.length; i--;) {
+            if (url.indexOf(configurations[i]['url']) !== -1) {
               return true;
             }
           }
-        }
-        return false;
-      };
+          return false;
+        };
 
-      /**
-       * Emit hide loader logic
-       *
-       * @param {object} config
-       * The response configuration
-       */
-      var checkAndHide = function (config) {
-        if (isUrlOnWhitelist(config.url) && isAllowedRequestType(config.headers) && (--numLoadings) === 0) {
-          $rootScope.$emit('loaderHide', config.method);
-        }
-      };
-
-      return {
         /**
-         * Broadcast the loader show event
+         * Checks if the request is allowed by the configuration.
+         *
+         * @param {object} config The request configuration
+         * @returns {boolean} True if the request is allowed, false otherwise.
+         */
+        var isAllowedRequest = function (config) {
+          if (isUrlOnWhitelist(config.url)) {
+            for (var i = configurations.length; i--;) {
+              if (config.url.indexOf(configurations[i]['url']) !== -1 && (config.headers[configurations[i]['headerName']] === configurations[i]['requestType'])) {
+                return true;
+              }
+            }
+          }
+          return false;
+        };
+
+        /**
+         * Emit hide loader logic
          *
          * @param {object} config
-         *
-         * @returns {object|Promise}
+         * The response configuration
          */
-        request: function (config) {
-
-          if (isUrlOnWhitelist(config.url) && isAllowedRequestType(config.headers)) {
-            numLoadings++;
-            $rootScope.$emit('loaderShow', config.method);
+        var checkAndHide = function (config) {
+          if (isAllowedRequest(config) && (--numLoadings) === 0) {
+            $rootScope.$emit('loaderHide', config.method);
           }
+        };
 
-          return config || $q.when(config);
-        },
+        return {
+          /**
+           * Broadcast the loader show event
+           *
+           * @param {object} config
+           *
+           * @returns {object|Promise}
+           */
+          request: function (config) {
+            if (isAllowedRequest(config)) {
+              numLoadings++;
+              $rootScope.$emit('loaderShow', config.method);
+            }
+            return config || $q.when(config);
+          },
 
-        /**
-         * Broadcast the loader hide event
-         *
-         * @param {object} response
-         *
-         * @returns {object|Promise}
-         */
-        response: function (response) {
-          checkAndHide(response.config);
+          /**
+           * Broadcast the loader hide event
+           *
+           * @param {object} response
+           *
+           * @returns {object|Promise}
+           */
+          response: function (response) {
+            checkAndHide(response.config);
 
-          return response || $q.when(response);
-        },
+            return response || $q.when(response);
+          },
 
-        /**
-         * Handle errors
-         *
-         * @param {object} response
-         *
-         * @returns {Promise}
-         */
-        responseError: function (response) {
-          checkAndHide(response.config);
+          /**
+           * Handle errors
+           *
+           * @param {object} response
+           *
+           * @returns {Promise}
+           */
+          responseError: function (response) {
+            checkAndHide(response.config);
 
-          return $q.reject(response);
-        }
-      };
-    }
-  ];
-})
+            return $q.reject(response);
+          }
+        };
+      }
+    ];
+  })
 
-.config(['$httpProvider', function ($httpProvider) {
-  $httpProvider.interceptors.push('httpMethodInterceptor');
-}]);
+  .config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.interceptors.push('httpMethodInterceptor');
+  }]);
